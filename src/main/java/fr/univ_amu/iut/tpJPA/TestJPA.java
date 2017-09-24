fd252061ce468805ffd4a1c65c1ec8efc1811978
Sébastien NEDJAR <sebastien@nedjar.com>
2017-09-24 17:12:14 +0200

    Début de la markdownisation du sujet

---
+++ src/main/java/fr/univ_amu/iut/tpJPA/TestJPA.java
@@ -0,0 +1,18 @@
+package fr.univ_amu.iut.tpJPA;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestJPA {
+	public static void main(String[] args) {
+		// Initializes the Entity manager
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("employePU");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		em.close();
+		emf.close();
+	}
+}
